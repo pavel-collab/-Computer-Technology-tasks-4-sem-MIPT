71dd846002f77cf3ebd19f9b0839211c8de4a66f
Pavel Filippenko <rubikpf2002@mail.ru>
2022-03-11 23:49:35 +0300

    Add CMakeLists.txt, Add gtests, but not all of them work correct

---
+++ test.cpp
@@ -0,0 +1,128 @@
+//* g++ test.cpp -std=c++17 -lgtest -lgtest_main -lpthread
+#include <iostream>
+#include <cmath> // for floor
+#include <numeric> // for std::gcd 
+#include <exception> // for exceptions
+#include <stdexcept> // for exceptions types
+#include <utility> // for std::rel_ops
+#include <gtest/gtest.h>
+#include "frac_class.hpp"
+
+TEST(ClassFraction, FractionGetters)
+{   
+    Fraction<int> a(1, 2);
+    Fraction<short> b(3, 5);
+    Fraction<long> x;
+    Fraction<long long> y(2000000000, 40000000000);
+    Fraction<int32_t> t(123, 76);
+    Fraction<unsigned> m(10, 100);
+
+    // --------------------------------------------------------------------------------------------
+    ASSERT_EQ(a.get_numerator(), 1);
+    ASSERT_EQ(a.get_denominator(), 2);
+    // --------------------------------------------------------------------------------------------
+    ASSERT_EQ(b.get_numerator(), 3);
+    ASSERT_EQ(b.get_denominator(), 5);
+    // --------------------------------------------------------------------------------------------
+    ASSERT_EQ(x.get_numerator(), 1);
+    ASSERT_EQ(x.get_denominator(), 1);
+    // --------------------------------------------------------------------------------------------
+    ASSERT_EQ(y.get_numerator(), 1);
+    ASSERT_EQ(y.get_denominator(), 20);
+    // --------------------------------------------------------------------------------------------
+    ASSERT_EQ(t.get_numerator(), 123);
+    ASSERT_EQ(t.get_denominator(), 76);
+    // --------------------------------------------------------------------------------------------
+    ASSERT_EQ(m.get_numerator(), 1);
+    ASSERT_EQ(m.get_denominator(), 10);
+    // --------------------------------------------------------------------------------------------
+}
+
+TEST(ClassFraction, FractionBasicOperators)
+{
+    Fraction<int> a(2, 3);
+    Fraction<int> b(3, 4);
+    Fraction<int> x(1, 2);
+    Fraction<int> y(8, 5);
+
+    a+=1;
+    ASSERT_EQ(a, Fraction<int>(5, 3));
+    a-=1;
+    ASSERT_EQ(a, Fraction<int>(2, 3));
+    a*=5;
+    ASSERT_EQ(a, Fraction<int>(10, 3));
+    b/=3;
+    ASSERT_EQ(b, Fraction<int>(1, 4));
+
+    a += b;
+    ASSERT_EQ(a, Fraction<int>(43, 12));
+    x -= b;
+    ASSERT_EQ(x, Fraction<int>(1, 4));
+    x *= b;
+    ASSERT_EQ(x, Fraction<int>(1, 16));
+    x /= y;
+    ASSERT_EQ(x, Fraction<int>(5, 16*8));
+}
+
+TEST(ClassFraction, FractionUnarOperators)
+{
+    Fraction<int64_t> a(1, 15);
+    Fraction<int64_t> b(25, 3);
+
+    ++a;
+    ASSERT_EQ(a, Fraction<int64_t>(16, 15));
+    a++;
+    ASSERT_EQ(a, Fraction<int64_t>(21, 15));
+    ++b;
+    ASSERT_EQ(b, Fraction<int64_t>(28, 3));
+    b++;
+    ASSERT_EQ(b, Fraction<int64_t>(31, 3));
+
+}
+
+TEST(ClassFraction, FractionBinarOperators)
+{
+    Fraction<short> a(1, 2);
+    Fraction<short> b(2, 3);
+    Fraction<short> d(5, 3);
+    Fraction<short> c(9, 10);
+
+    auto x = b + 3;
+    auto y = a - 1;
+    auto z = d * 3;
+    auto t = c / 9;
+    EXPECT_EQ(x, Fraction<short>(11, 3));
+    EXPECT_EQ(y, Fraction<short>(-1, 2));
+    EXPECT_EQ(d, Fraction<short>(5, 1));
+    EXPECT_EQ(c, Fraction<short>(1, 10));
+
+    auto m = y / x;
+    EXPECT_EQ(m, Fraction<short>(-3, 2));
+    auto n = d + c;
+    EXPECT_EQ(n, Fraction<short>(51, 10));
+    auto r = x - y;
+    EXPECT_EQ(r, Fraction<short>(25, 6));
+    auto p = y * d;
+    EXPECT_EQ(p, Fraction<short>(-5, 2));
+}
+
+//! дописать тесты на операторы сравнения
+
+TEST(ClassFraction, FractionOtherMethods)
+{
+    Fraction<unsigned> a(1, 2);
+    Fraction<int> b(-5, 20);
+
+    EXPECT_EQ(a.decimal(), 0.5);
+    EXPECT_EQ(b.decimal(), -0.25);
+
+    double x = 0.125;
+    double y = 0.3;
+    double z = 0.203;
+
+    Fraction<int> t;
+
+    ASSERT_EQ(t.dec_to_reg(x), Fraction<int>(1, 8));
+    ASSERT_EQ(t.dec_to_reg(y), Fraction<int>(3, 10));
+    ASSERT_EQ(t.dec_to_reg(z), Fraction<int>(203, 1000));
+}
