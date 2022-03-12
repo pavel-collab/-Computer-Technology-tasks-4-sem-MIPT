702a07c5cf8c6f66d6e70d4dee13b7daca7eba4e
Pavel Filippenko <rubikpf2002@mail.ru>
2022-03-13 00:04:00 +0300

    Wrote Fraction class on python: improve constructor, overload operators. Need to write some other methods for class (goto c++ realisation). Add Readme file.

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Readme
+
+Study project -- realisation of class "Fraction" on C++ and python.
+
+## C++ realisation
+
+To build the project use:
+```
+mkdir build && cd build
+cmake ..
+make
+```
+If you want to run main file, use:
+```
+./main
+```
+If you want to run unit tests, use:
+```
+./runUnitTests
+```
+
+## Python realisation
+
+Firstly you need to give access to execute to your file frac_class.py:
+```
+chmod +x frac_class.py
+```
+To run executable file, use:
+```
+./frac_class.py
+```
--- frac_class.py
+++ frac_class.py
@@ -1,17 +1,24 @@
 #! /usr/bin/env python3
+from math import gcd
 
 class Fraction:
     __slots__ = ['__numerator', '__denominator']
 
-    ## Constructor
+    #----------------------------------------------------Constructor----------------------------------------------------
+    
     def __init__(self, N = 1, D = 1):
-        self.__numerator = N
-        self.__denominator = D
-        print('Constructor called for', '[' + str(hex(id(self))) + ']')
-    ## Distructor
-    def __del__(self):
-        print('Destrictor called for', '[' + str(hex(id(self))) + ']')
+        try:
+            empty = N / D
+        except ZeroDivisionError as err:
+            print(err)
 
+        GCD = gcd(N, D)
+        self.__numerator = int(N / GCD)
+        self.__denominator = int(D / GCD)
+        print("Constructor called")
+
+    #----------------------------------------------------Getters----------------------------------------------------
+    
     @property # <- применяется для гетеров   
     def numerator(self):
         return self.__numerator
@@ -19,12 +26,237 @@ class Fraction:
     def denominator(self):
         return self.__denominator
 
+    #----------------------------------------------------Output----------------------------------------------------
+
+    ## overloading output
+    def __str__(self):
+        return '({})/({})'.format(self.__numerator, self.__denominator)
+
+    #----------------------------------------------------Overloading of binary operators----------------------------------------------------
+
+    ## overloading operator "+"
+    def __add__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(
+                self.__numerator * other.__denominator + self.__denominator * other.__numerator,
+                self.__denominator * other.__denominator
+            )
+        elif isinstance(other, int):
+            return Fraction(
+                self.__numerator + other * self.__denominator,
+                self.__denominator
+            )
+        else:
+            return NotImplemented
+
+    ## overloading operator "-"
+    def __sub__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(
+                self.__numerator * other.__denominator - self.__denominator * other.__numerator,
+                self.__denominator * other.__denominator
+            )
+        elif isinstance(other, int):
+            return Fraction(
+                self.__numerator - other * self.__denominator,
+                self.__denominator
+            )
+        else:
+            return NotImplemented
+
+    ## overloading operator "*"
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(
+                self.__numerator * other.__numerator,
+                self.__denominator * other.__denominator
+            )
+        elif isinstance(other, int):
+            return Fraction(
+                other * self.__numerator,
+                self.__denominator
+            )
+        else:
+            return NotImplemented
+
+    ## overloading operator "/"
+    def __truediv__(self, other):
+        if isinstance(other, Fraction):
+            try:
+                empty = 1 / other.__numerator
+            except ZeroDivisionError as err:
+                print(err)
+
+            return Fraction(
+                self.__numerator * other.__numerator,
+                self.__denominator * other.__denominator
+            )
+        elif isinstance(other, int):
+            try:
+                empty = 1 / other
+            except ZeroDivisionError as err:
+                print(err)
+            return Fraction(
+                self.__numerator - other * self.__denominator,
+                self.__denominator
+            )
+        else:
+            return NotImplemented
+    
+    ## overloading operator "**"
+    def __pow__(self, other):
+        if isinstance(other, int):
+            return Fraction(
+                self.__numerator ** other ,
+                self.__denominator ** other
+            )
+        else:
+            return NotImplemented
+    
+    #----------------------------------------------------Overloading of unar operators----------------------------------------------------
+
+    def __iadd__(self, other):
+        if isinstance(other, Fraction):
+            self.__numerator = self.__numerator * other.__denominator + self.__denominator * other.__numerator
+            self.__denominator = self.__numerator * other.__denominator
+            return self
+        elif isinstance(other, int):
+            self.__numerator += other * self.__denominator
+            return self
+        else:
+            return NotImplemented
+
+    def __isub__(self, other):
+        if isinstance(other, Fraction):
+            self.__numerator = self.__numerator * other.__denominator - self.__denominator * other.__numerator
+            self.__denominator = self.__numerator * other.__denominator
+            return self
+        elif isinstance(other, int):
+            self.__numerator -= other * self.__denominator
+            return self
+        else:
+            return NotImplemented
+    
+    def __imul__(self, other):
+        if isinstance(other, Fraction):
+            self.__numerator *= other.__numerator
+            self.__denominator *= other.__denominator
+            return self
+        elif isinstance(other, int):
+            self.__numerator *= other
+            return self
+        else:
+            return NotImplemented
+
+    def __itruediv__(self, other):
+        if isinstance(other, Fraction):
+            try:
+                empty = 1 / other.__numerator
+            except ZeroDivisionError as err:
+                print(err)
+
+            self.__numerator *= other.__denominator
+            self.__denominator *= other.__numerator
+            return self
+        elif isinstance(other, int):
+            try:
+                empty = 1 / other
+            except ZeroDivisionError as err:
+                print(err)
+
+            self.__denominator *= other
+            return self
+        else:
+            return NotImplemented
+
+    #----------------------------------------------------Overloading of right operators----------------------------------------------------
+
+    def __radd__(self, other):
+        return Fraction(
+            self.__numerator + other * self.__denominator,
+            self.__denominator
+        )
+
+    def __rsub__(self, other):
+        return Fraction(
+            other * self.__denominator - self.__numerator,
+            self.__denominator
+        )
+    
+    def __rmul__(self, other):
+        return Fraction(
+            other * self.__numerator,
+            self.__denominator
+        )
+
+    def __rtruediv__(self, other):
+        try:
+            empty = 1 / self.__denominator
+        except ZeroDivisionError as err:
+            print(err)
+
+        return Fraction(
+            other * self.__denominator,
+            self.__numerator
+        )   
+
+    #----------------------------------------------------Overloading of compare operators----------------------------------------------------  
+
+    def __eq__(self, other):
+        if isinstance(other, Fraction):
+            return self.__numerator * self.__denominator == self.__denominator * other.__numerator
+        elif isinstance(other, int):
+            return self.__numerator == self.__denominator * other
+        else:
+            return NotImplemented
+    
+    #! не факт, что истинность == означает ложность !=
+    #! поэтому перегружаем оба оператора
+    def __ne__(self, other):
+        if isinstance(other, Fraction):
+            return self.__numerator * self.__denominator != self.__denominator * other.__numerator
+        elif isinstance(other, int):
+            return self.__numerator != self.__denominator * other
+        else:
+            return NotImplemented
+    
+    def __gt__(self, other):
+        if isinstance(other, Fraction):
+            return self.__numerator * self.__denominator > self.__denominator * other.__numerator
+        elif isinstance(other, int):
+            return self.__numerator > self.__denominator * other
+        else:
+            return NotImplemented
+    
+    def __lt__(self, other):
+        return other > self
+    
+    def __ge__(self, other):
+        return self > other or self == other
+    
+    def __le__(self, other):
+        return other > self or self == other
+
+    #----------------------------------------------------Some other methods for class Fraction----------------------------------------------------
+
+    #TODO: write some other methods (goto c++ realisation)
 
 def main():
-    print('hello, world!')
-    a = Fraction(1, 2)
-    print(a.numerator)
-    print(a.denominator)
+    x = Fraction(1, 2)
+    y = Fraction(3, 2)
+    z = Fraction(2, 4)
+
+    if (x >= z):
+        print(1)
+        print(x)
+        print(y)
+    else:
+        print(2)
+        print(x)
+        print(y)
 
 if __name__ == '__main__':
     main()
+
+#??? кривая реализция проверки при делении на 0 в конструкторе и в операторе (создаю empty = N / D)
+#??? перегрузил операторы типа __radd__ (когда член класса стоит справа от "+", а слагаемое слева не является членом класса), почему в питоне так дофига надо перегружать самомму
